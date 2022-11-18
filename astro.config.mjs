45eacb7e2eaecd2c261c67cf87dbaebe84d6caf4
xTalAlex <talale93@hotmail.it>
2022-11-18 04:12:23 +0100

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,19 @@
 import { defineConfig } from 'astro/config';
 import Config from './src/site.config.js';
 import tailwind from '@astrojs/tailwind';
-
 import vue from "@astrojs/vue";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: Config.url,
-  integrations: [tailwind({
-    config: {
-      applyBaseStyles: false
-    }
-  }), vue()]
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false
+      }
+    }), 
+    vue(), 
+    sitemap()
+  ]
 });
