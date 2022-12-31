d9a0bad3de8687e1c00bda81f70194c23abb1e9e
xTalAlex <talale93@hotmail.it>
2022-12-31 04:45:28 +0100

    improvements

--- src/utils.js
+++ src/utils.js
@@ -35,4 +35,16 @@ const toTrimmedAlphanumeric = (inputString) => {
     return inputString.replace(/[^a-zA-Z0-9 ]/g, '').trim();
 }
 
+const slugify = ( text ) => {
+    return text
+        .toString()
+        .normalize( 'NFD' )                   // split an accented letter in the base letter and the acent
+        .replace( /[\u0300-\u036f]/g, '' )   // remove all previously split accents
+        .toLowerCase()
+        .trim()
+        .replace(/\s+/g, '-')
+        .replace(/[^\w\-]+/g, '')
+        .replace(/\-\-+/g, '-'); 
+};
+
 export { formatDate, adjacentCells, delayFunction, toTrimmedAlphanumeric };
