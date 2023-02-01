c66e3cd24ed543f14674f0943d6a77a868ee0218
xTalAlex <talale93@hotmail.it>
2023-02-01 20:28:13 +0100

    quickSearch

--- src/components/vue/Wishlist.vue
+++ src/components/vue/Wishlist.vue
@@ -3,8 +3,10 @@
         <div v-if="items.length" class="mt-12 carousel carousel-start w-full p-4 space-x-4 overflow-y-visible bg-neutral rounded-box">
             <div v-for="item in items" v-bind:key="item.name" 
                 id="slide1" class="carousel-item relative first:pl-4">
-                <img :src="item.imgSrc" :alt="item.name" class="rounded-box" />
-                <button class="btn glass btn-xs absolute -right-1 -top-1"
+                <img :src="item.imgSrc" :alt="item.name" class="cursor-pointer rounded-box"
+                    @click="searchGame(item.id)"
+                />
+                <button class="btn glass btn-xs absolute -right-1 -top-1 z-10"
                     @click="removeWishlistItem(item)"
                 >x</button>
             </div>
@@ -23,5 +25,11 @@
     const items = computed(() => {
         return Object.values($wishlist.value["items"]);
     })
+
+    function searchGame(slug){
+        document.dispatchEvent(
+            new CustomEvent("quickSearch", { detail: slug })
+        );
+    };
 </script>
 
