ae571f978aa1f93a35b63d13b04fc68d0a679c74
xTalAlex <talale93@hotmail.it>
2023-05-14 13:02:18 +0200

    paypal donation

--- src/components/vue/UserCard.vue
+++ src/components/vue/UserCard.vue
@@ -1,21 +1,19 @@
 <template>
-    <div
-        class="mx-auto bg-base-100 rounded-box col-span-3 row-span-3 mx-2 grid w-72 flex-shrink-0 place-items-center items-center gap-4 p-4 py-8 shadow-xl xl:mx-0 xl:w-full svelte-1n6ue57">
+    <div class="mx-auto bg-base-100 rounded-box col-span-3 row-span-3 grid w-72 flex-shrink-0 place-items-center items-center gap-4 p-4 py-8 shadow-xl xl:mx-0 xl:w-full svelte-1n6ue57"
+        v-if="$isLogged"
+    >
         <div class="dropdown">
             <div tabindex="0">
                 <div class="avatar" :class="{ 'online': $isLogged, 'offline': !$isLogged }">
                     <div class="mask mask-squircle bg-base-content h-24 w-24 bg-opacity-10 p-px"><img
-                            :src="$userInfo.avatar" width="94" height="94" alt="Avatar Tailwind CSS Component"
-                            class="mask mask-squircle"></div>
+                        :src="$userInfo.avatar" width="94" height="94" alt="Avatar Tailwind CSS Component"
+                        class="mask mask-squircle"></div>
                 </div>
             </div>
             <div tabindex="0" class="dropdown-content py-2">
                 <div class="card compact bg-neutral-focus text-neutral-content rounded-box w-72 shadow-xl">
                     <div class="card-body">
-                        <h2 class="card-title font-extrabold capitalize">avatar component</h2>
-                        <p class="text-neutral-content text-sm text-opacity-80">Use avatar component with any size</p>
-                        <div class="mt-4 flex justify-end"><a href="/components/avatar"
-                                class="btn btn-primary btn-sm xl:btn-md">See component</a></div>
+                        <h2 class="card-title font-extrabold capitalize">ID - {{ $userInfo.id }}</h2>
                     </div>
                 </div>
             </div>
@@ -27,20 +25,7 @@
                         <div class="text-lg font-extrabold">{{ $userInfo.name }}</div>
                         <div class="text-base-content/70 my-3 text-sm">{{ $userInfo.email }}
                             <br>
-                            Global Illustration Observer
-                            <br>
-                            Business Alignment Developer
-                        </div>
-                    </div>
-                </div>
-                <div tabindex="0" class="dropdown-content py-2">
-                    <div class="card compact bg-neutral-focus text-neutral-content rounded-box w-72 shadow-xl">
-                        <div class="card-body">
-                            <h2 class="card-title font-extrabold capitalize">card component</h2>
-                            <p class="text-neutral-content text-sm text-opacity-80">Card component is used to show products,
-                                features and more.</p>
-                            <div class="mt-4 flex justify-end"><a href="/components/card"
-                                    class="btn btn-primary btn-sm xl:btn-md">See component</a></div>
+                            {{  $userInfo.confirmed_at ? 'Email verificata' : 'Email non verificata' }}
                         </div>
                     </div>
                 </div>
@@ -48,19 +33,11 @@
             <div class="dropdown w-full">
                 <div tabindex="0">
                     <div class="mt-2 text-center">
-                        <div class="badge badge-ghost">Design</div>
-                        <div class="badge badge-ghost">Art</div>
-                        <div class="badge badge-ghost">Illustration</div>
-                    </div>
-                </div>
-                <div tabindex="0" class="dropdown-content py-2">
-                    <div class="card compact bg-neutral-focus text-neutral-content rounded-box w-72 shadow-xl">
-                        <div class="card-body">
-                            <h2 class="card-title font-extrabold capitalize">badge component</h2>
-                            <p class="text-neutral-content text-sm text-opacity-80">Use badge component to highlight small
-                                inline items</p>
-                            <div class="mt-4 flex justify-end"><a href="/components/badge"
-                                    class="btn btn-primary btn-sm xl:btn-md">See component</a></div>
+                        <div class="badge badge-ghost">
+                            Iscrizione  {{ formatDate($userInfo.created_at) }}
+                        </div>
+                        <div class="badge badge-ghost">
+                            Modifica {{ formatDate($userInfo.updated_at) }}
                         </div>
                     </div>
                 </div>
@@ -68,27 +45,25 @@
         </div>
         <div class="dropdown dropdown-top">
             <div tabindex="0">
-                <div class="btn-group"><button class="btn btn-accent btn-sm">Follow</button> <button
+                <div class="btn-group">
+                    <button class="btn btn-accent btn-sm">
+                        {{ $userInfo.isAdmin ? 'ADMIN' : 'USER' }}
+                    </button> 
+                    <button
                         aria-label="button component" class="btn btn-accent btn-square btn-sm"><svg
                             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                             class="h-6 w-6 stroke-current">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                 d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z">
                             </path>
-                        </svg></button></div>
-            </div>
-            <div tabindex="0" class="dropdown-content py-2">
-                <div class="card compact bg-neutral-focus text-neutral-content rounded-box w-72 shadow-xl">
-                    <div class="card-body">
-                        <h2 class="card-title font-extrabold capitalize">button group component</h2>
-                        <p class="text-neutral-content text-sm text-opacity-80">Use it to group multiple buttons together
-                        </p>
-                        <div class="mt-4 flex justify-end"><a href="/components/button-group"
-                                class="btn btn-primary btn-sm xl:btn-md">See component</a></div>
-                    </div>
+                        </svg>
+                    </button>
                 </div>
             </div>
         </div>
+    </div>
+    <div class="grid place-items-center" v-else>
+        <a class="link" @click="login">Effettua il Log in per vedere il tuo profilo</a>
     </div>
 </template>
 
@@ -96,8 +71,13 @@
 import { ref, computed, onMounted } from "vue";
 import { useStore } from '@nanostores/vue';
 import { isLogged, userInfo } from "@lib/authStore";
+import { formatDate } from "src/utils";
 
 const $isLogged = useStore(isLogged);
 const $userInfo = useStore(userInfo);
+
+function login(){
+  netlifyIdentity.open();
+}
 
 </script>
--- src/lib/authStore.js
+++ src/lib/authStore.js
@@ -2,25 +2,56 @@ import { atom, map } from 'nanostores';
 
 export const isLogged = atom(false);
 
+/**
+ * 
+ *  user
+ *      id
+ *      email
+ *      confirmed_at
+ *      created_at
+ *      updated_at
+ *      token
+ *          access_token
+ *      user_metadata
+ *          full_name
+ *          avatar_url
+ *      app_metadata
+ *          provider
+ *          roles
+ *      url
+ */
+
 export const userInfo = map({
-    name: null, // user.user_metadata.full_name
-    email: null, // user.email
-    isAdmin: false, // user.app_metadata.roles
-    avatar: null, // user.user_metadata.avatar_url
+    id: null,
+    email: null,
+    confirmed_at: null,
+    created_at: null,
+    updated_at: null,
+    name: null,
+    avatar: null,
+    isAdmin: false,
 })
 
 export function loginUser( user ) {
-    userInfo.setKey("name", user.user_metadata?.full_name);
+    userInfo.setKey("id", user.id);
     userInfo.setKey("email", user.email);
-    userInfo.setKey("isAdmin", user.app_metadata?.roles?.includes('admin'));
-    userInfo.setKey("avatar", user.user_metadata?.avatar_url ?? 'https://robohash.org/' + encodeURIComponent($userInfo.value.email) + '.png?bgset=bg1' );
+    userInfo.setKey("confirmed_at", user.confirmed_at);
+    userInfo.setKey("created_at", user.created_at);
+    userInfo.setKey("updated_at", user.updated_at);
+    userInfo.setKey("name", user.user_metadata?.full_name);
+    userInfo.setKey("avatar", user.user_metadata?.avatar_url ?? 'https://robohash.org/' + encodeURIComponent(user.email) + '.png?bgset=bg1' );
+    userInfo.setKey("isAdmin", user.app_metadata?.roles?.includes('admin'));    
     isLogged.set(true);
 }
 
 export function logoutUser() {
     isLogged.set(false);
-    userInfo.setKey("name", null);
+    userInfo.setKey("id", null);
     userInfo.setKey("email", null);
-    userInfo.setKey("isAdmin", false);
-    userInfo.setKey("avatar", null);
+    userInfo.setKey("confirmed_at", null);
+    userInfo.setKey("created_at", null);
+    userInfo.setKey("updated_at", null);
+    userInfo.setKey("name", null);
+    userInfo.setKey("avatar", null );
+    userInfo.setKey("isAdmin", false);  
 }
