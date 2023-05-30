bcecaa701b2e7c7181ac2a48945be95bdf356d01
xTalAlex <talale93@hotmail.it>
2023-05-30 20:14:16 +0200

    fixes

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,16 +3,10 @@ import { z, defineCollection } from 'astro:content';
 import { rssSchema } from '@astrojs/rss';
 
 // 2. Define your collection(s)
-const postCollection = defineCollection({ 
-    schema: z.object({
-        title: z.string(),
-        date: z.date(),
-        featuredImage: z.string(),
-        gallery: z.array(z.string()).optional(),
-  })
-});
-
 const projectCollection = defineCollection({ 
+    // schema: rssSchema.extend({
+    //   gallery: z.array(z.string()).optional()
+    // }),
     schema: z.object({
         title: z.string(),
         pubDate: z.date(),
@@ -25,6 +19,5 @@ const projectCollection = defineCollection({
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
-  'posts': postCollection,
   'projects': projectCollection,
 };
