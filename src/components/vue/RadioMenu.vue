901c238cb992b0be95c0334f0f20a0f805905524
xTalAlex <talale93@hotmail.it>
2023-06-05 07:48:32 +0200

    web radio

---
+++ src/components/vue/RadioMenu.vue
@@ -0,0 +1,79 @@
+<template>
+    <ul class="menu glass rounded-box fixed top-10 z-30 transition right-0 hover:right-1 translate-x-14 group hover:translate-y-10 hover:-translate-x-0 -rotate-90 hover:rotate-0">
+        <li>
+            <div class="tooltip-left tooltip" v-bind:data-tip="curStation.name" :class="{ 'text-accent' : isPlaying}">
+                <svg class="rotate-90 group-hover:rotate-0 stroke-current" :class="isPlaying ? 'h-6 w-6 animate-pulse' : 'h-6 w-6 animate-pulse'" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="M22 2h-2v2h2v12h-2v2h2v-2h2V4h-2V2ZM2 4H0v12h2v2h2v-2H2V4Zm0 0V2h2v2H2Zm4 2H4v8h2V6Zm0 0V4h2v2H6Zm4 0h4v2h-4V6Zm0 6H8V8h2v4Zm4 0h-4v2H8v4H6v4h2v-4h2v-4h4v4h2v4h2v-4h-2v-4h-2v-2Zm0 0h2V8h-2v4Zm6-6h-2V4h-2v2h2v8h2V6Z"/></svg>
+            </div>
+        </li>
+        <li>
+            <div class="dropdown dropdown-hover dropdown-left p-0">
+                <label class="cursor-pointer px-4 py-3 h-full w-full" tabindex="0">
+                    <svg class="h-6 w-6 stroke-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="M10 13h6V5h6v4h-4v10h-8v-6zm2 2v2h4v-2h-4zM2 17h6v2H2v-2zm6-4H2v2h6v-2zM2 9h12v2H2V9zm12-4H2v2h12V5z"/></svg>
+                </label>
+                <ul tabindex="0"
+                    class="-mr-1 -mt-12 dropdown-content menu text-sm font-semibold shadow-xl bg-base-200 rounded-box w-52">
+                    <li v-for="station in stations">
+                        <a v-on:click="changeStation(station)" :class="{ 'active' : curStation.name == station.name }">
+                            <img class="h-8 w-8" v-bind:src="station.imgSrc" />{{ station.name }}
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </li>
+        <li>
+            <button class="" v-on:click="toggle">
+                <!-- Play -->
+                <svg class="h-5 w-5 stroke-current" :class="{'hidden' : isPlaying}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="M10 20H8V4h2v2h2v3h2v2h2v2h-2v2h-2v3h-2v2z"/></svg>
+                <!-- Pause -->
+                <svg class="h-5 w-5 stroke-current" :class="{'hidden' : !isPlaying}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="M10 4H5v16h5V4zm9 0h-5v16h5V4z"/></svg>
+            </button>
+            <audio class="hidden" ref="player" src=""></audio>
+        </li>
+    </ul>
+</template>
+
+<script setup>
+import { stations } from '@data/radio-stations';
+import { useStorage } from "@lib/useStorage";
+
+import { ref, reactive, computed, onMounted } from "vue";
+
+let open = ref(false);
+let curStation = reactive({name: "Ascolta una radio!"});
+let isPlaying = ref(false);
+const lastRadioStation = useStorage('lastRadioStation', stations[0]);
+
+const player = ref(null)
+
+function changeStation(station, startPlaying = true) {
+    pause();
+    curStation = station;
+    lastRadioStation.value = station;
+    player.value.src = station.url;
+    if(startPlaying) play();
+}
+
+function play() {
+    if (player.value.paused) {
+        player.value.play();
+        isPlaying.value = true;
+        lastRadioStation.value.isPlaying = true;
+    }
+}
+
+function pause() {
+    if (!player.value.paused) {
+        player.value.pause();
+        isPlaying.value = false;
+        lastRadioStation.value.isPlaying = false;
+    }
+}
+
+function toggle() {
+    player.value.paused ? play() : pause();
+}
+
+onMounted(() => {
+    changeStation(lastRadioStation.value, lastRadioStation.value.isPlaying);
+});
+</script>
---
+++ src/data/radio-stations.js
@@ -0,0 +1,19 @@
+const stations = [
+    {
+        name: "Rai Radio 2",
+        url: "http://icestreaming.rai.it/2.mp3",
+        imgSrc: "/images/radio/rr2.svg",
+    },
+    {
+        name: "LatteMiele",
+        url: "https://sr15.inmystream.it/stream/lattemiele",
+        imgSrc: "/images/radio/lattemiele.svg",
+    },
+    {
+        name: "Radio Cuore",
+        url: "https://stream10.xdevel.com/audio32s975552-1839/stream/icecast.audio",
+        imgSrc: "/images/radio/cuore.png",
+    }
+];
+
+export { stations };
