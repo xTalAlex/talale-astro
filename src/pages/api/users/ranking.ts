23d011a42177ec913b769f5f66c48b00f22cdd5c
xTalAlex <talale93@hotmail.it>
2024-04-17 22:17:18 +0200

    fix

--- src/pages/api/users/ranking.ts
+++ src/pages/api/users/ranking.ts
@@ -4,6 +4,7 @@ import { db, User } from "astro:db";
 export const prerender = false;
 
 export const GET: APIRoute = async (ctx) => {
-  const users = await db.select().from(User);
+  //const users = await db.select().from(User);
+  const users = {};
   return new Response(JSON.stringify(users));
 };
