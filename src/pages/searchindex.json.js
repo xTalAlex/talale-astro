73f52483f71c5ff22ed2e876465c55500c025c31
xTalAlex <talale93@hotmail.it>
2024-04-21 22:44:10 +0200

    pages refactoring

--- src/pages/searchindex.json.js
+++ src/pages/searchindex.json.js
@@ -1,10 +1,5 @@
 import { games } from "@data/gamesCatalog";
 
 export async function GET() {
-  return new Response(JSON.stringify(games), {
-    status: 200,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+  return new Response(JSON.stringify(games));
 }
