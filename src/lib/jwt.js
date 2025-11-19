2c5b40feef3d0780b02c9db5c7def23a3113ec7b
Alessandro Talamona <info@talale.it>
2025-11-19 12:43:01 +0100

    JWT verification for API endpoints

---
+++ src/lib/jwt.js
@@ -0,0 +1,30 @@
+import { createRemoteJWKSet, jwtVerify } from "jose";
+
+export const verifyAuth0Token = async (request, issuer, audience) => {
+  const token = extractBearerToken(request);
+
+  // JWKS (JSON Web Key Set) contains public keys for token verification
+  const JWKS = createRemoteJWKSet(new URL(".well-known/jwks.json", issuer));
+
+  const result = await jwtVerify(token, JWKS, {
+    issuer: issuer,
+    audience: audience,
+  });
+
+  return { token, result };
+};
+
+const extractBearerToken = (request) => {
+  const authorization = request.headers.get("Authorization") ?? "";
+
+  const [type, token, ...parts] = authorization
+    .replace(/\s+/g, " ") // Replace multiple spaces with a single space
+    .trim()
+    .split(" ");
+
+  if (type !== "Bearer" || parts.length !== 0) {
+    throw new Error("Missing or invalid Authorization header");
+  }
+
+  return token;
+};
